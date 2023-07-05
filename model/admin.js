020fe9cb340debc66f797ab1c3acfb6a802d3ff6
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-07-05 21:22:21 +0530

    schema created

---
+++ model/admin.js
@@ -0,0 +1,15 @@
+const { model, Schema } = require("mongoose");
+
+const adminSchema = new Schema({
+  admin_name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Admin = model("admin", adminSchema);
+module.exports = Admin;
